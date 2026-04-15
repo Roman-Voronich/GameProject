5b48e8af1bfe963730514663156498db7399f55b
rfdyerb <chuxarevy@gmail.com>
2026-04-15 11:56:35 +0500

    version 0.0.1

---
+++ Scripts/SctileMapLayer.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class SctileMapLayer : TileMapLayer
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// for (var x = 0; x < 40; x++)
+		// 	for (var y = 0; y < 30; y++)
+		// 		SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
