71e6a7cb44048ae0a0b1325ce65134fa0ce9df2b
rfdyerb <chuxarevy@gmail.com>
2026-04-22 10:13:09 +0500

    генерация и редактирование карты v1, добавление zoom

---
+++ CustomNodes/MapFromNoise.cs
@@ -0,0 +1,81 @@
+using Godot;
+using System;
+
+[Tool, GlobalClass]
+public partial class MapFromNoise : TileMapLayer
+{
+    private NoiseTexture2D _noiseTexture;
+    private int _width = 50;
+    private int _height = 50;
+    private int _seed = 10;
+
+    [Export]
+    public NoiseTexture2D noiseTexture
+    {
+        get => _noiseTexture;
+        set
+        {
+            _noiseTexture = value;
+            if (Engine.IsEditorHint())
+                GenerateMap();
+        }
+    }
+
+    [Export]
+    public int width
+    {
+        get => _width;
+        set
+        {
+            _width = value;
+            if (Engine.IsEditorHint())
+                GenerateMap();
+        }
+    }
+
+    [Export]
+    public int height
+    {
+        get => _height;
+        set
+        {
+            _height = value;
+            if (Engine.IsEditorHint())
+                GenerateMap();
+        }
+    }
+
+    [Export]
+    public int seed
+    {
+        get => _seed;
+        set
+        {
+            _seed = value;
+            if (Engine.IsEditorHint())
+                GenerateMap();
+        }
+    }
+
+    public override void _Ready()
+    {
+        if (Engine.IsEditorHint())
+            GenerateMap();
+    }
+
+    private void GenerateMap()
+    {        
+        Clear();
+        var noise = _noiseTexture.Noise as FastNoiseLite;
+        noise.Seed = _seed;
+        for (var x = 0; x < _width; x++)
+            for (var y = 0; y < _height; y++)
+            {
+                var value = noise.GetNoise2D(x, y);
+                var atlasCoords = value > 0.2 ? new Vector2I(1, 1) :
+                                  value > -0.2 ? new Vector2I(1, 0) :
+                                  new Vector2I(0, 0);
+                SetCell(new Vector2I(x, y), 0, atlasCoords);
+            }
+    }
+}
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,15 +3,47 @@ using System;
 
 public partial class Player : Node2D
 {
+	[Export]
 	private int Speed = 10;
+	private MapFromNoise _tileMap;
+	private Camera2D camera;
+	private Vector2I cameraSize;
+	private float cameraZoom = 1;
+
+	private bool isBuildMode = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_tileMap = GetNode<MapFromNoise>(new NodePath("../Map"));
+		camera = GetNode<Camera2D>(new NodePath("Camera2D"));
+		cameraSize = GetTree().Root.Size;
 	}
 
     public override void _Input(InputEvent @event)
-    {
-    }
+	{
+		if (@event.IsActionPressed("ui_change_mode"))
+			changeMode();
+		if (isBuildMode
+			&& @event is InputEventMouseButton mouse
+			&& mouse.Pressed)
+			changeMap(mouse);
+	}
+
+	private void changeMap(InputEventMouseButton mouse)
+	{
+		var mouseGlobalPos = camera.GetGlobalMousePosition();
+		var tilePos = _tileMap.LocalToMap(_tileMap.ToLocal(mouseGlobalPos));
+		if (mouse.ButtonIndex == MouseButton.Left)
+			_tileMap.SetCell(tilePos, 0, new Vector2I(1, 0));
+		else if (mouse.ButtonIndex == MouseButton.Right)
+			_tileMap.SetCell(tilePos, 0, new Vector2I(1, 1));
+	}
+
+	private void changeMode()
+	{
+		isBuildMode = !isBuildMode;
+		GD.Print(isBuildMode, camera.Scale);
+	}
 
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,9 +52,14 @@ public partial class Player : Node2D
 		var direction = new Vector2();
 		direction.X = Input.GetAxis("ui_left", "ui_right");
 		direction.Y = Input.GetAxis("ui_up", "ui_down");
-
+		if (Input.IsActionJustPressed("ui_mouse_scroll_down"))
+			cameraZoom = Math.Max(cameraZoom * 0.8f, 0.5f);
+		if (Input.IsActionJustPressed("ui_mouse_scroll_up"))
+			cameraZoom = Math.Min(cameraZoom * 1.25f, 2f);
+		
+		camera.Zoom = new Vector2(1, 1) * cameraZoom;
 		direction.Normalized();
 
-		Position += direction * Speed;
+		Position += direction * Speed / cameraZoom;
 	}
 }
