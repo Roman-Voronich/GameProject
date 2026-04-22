bf5c8c36eb5c2210cde059eb524268b8015783af
rfdyerb <chuxarevy@gmail.com>
2026-04-22 13:02:14 +0500

    рефакторинг кода

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -9,6 +9,7 @@ public partial class Player : Node2D
 	private Camera2D camera;
 	private Vector2I cameraSize;
 	private float cameraZoom = 1;
+	private Vector2I currentTile = new(0, 0);
 
 	private bool isBuildMode = false;
 	// Called when the node enters the scene tree for the first time.
@@ -23,43 +24,60 @@ public partial class Player : Node2D
 	{
 		if (@event.IsActionPressed("ui_change_mode"))
 			changeMode();
-		if (isBuildMode
-			&& @event is InputEventMouseButton mouse
-			&& mouse.Pressed)
-			changeMap(mouse);
 	}
 
-	private void changeMap(InputEventMouseButton mouse)
+	private void changeMap()
 	{
 		var mouseGlobalPos = camera.GetGlobalMousePosition();
 		var tilePos = _tileMap.LocalToMap(_tileMap.ToLocal(mouseGlobalPos));
-		if (mouse.ButtonIndex == MouseButton.Left)
-			_tileMap.SetCell(tilePos, 0, new Vector2I(1, 0));
-		else if (mouse.ButtonIndex == MouseButton.Right)
-			_tileMap.SetCell(tilePos, 0, new Vector2I(1, 1));
+		_tileMap.SetCell(tilePos, 0, currentTile);
 	}
 
 	private void changeMode()
 	{
 		isBuildMode = !isBuildMode;
-		GD.Print(isBuildMode, camera.Scale);
+	}
+
+	private void changeTile()
+	{
+		var x = currentTile.X;
+		var y = currentTile.Y;
+		currentTile = new Vector2I(x + 1, y + (x + 1) % 2) % 2;
 	}
 
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var direction = new Vector2();
-		direction.X = Input.GetAxis("ui_left", "ui_right");
-		direction.Y = Input.GetAxis("ui_up", "ui_down");
+		DoMove();
+		DoZoom();
+		DoBuild();
+	}
+
+	private void DoBuild()
+	{
+		if (isBuildMode
+			&& Input.IsActionPressed("ui_left_click"))
+			changeMap();
+		if (Input.IsActionJustPressed("ui_change_tile"))
+			changeTile();
+	}
+
+	private void DoZoom()
+	{
 		if (Input.IsActionJustPressed("ui_mouse_scroll_down"))
 			cameraZoom = Math.Max(cameraZoom * 0.8f, 0.5f);
 		if (Input.IsActionJustPressed("ui_mouse_scroll_up"))
 			cameraZoom = Math.Min(cameraZoom * 1.25f, 2f);
-		
-		camera.Zoom += new Vector2(1, 1) * (cameraZoom - camera.Zoom.X) * 0.2f;
-		direction.Normalized();
+		camera.Zoom += Vector2.One * (cameraZoom - camera.Zoom.X) * 0.2f;
+	}
 
+	private void DoMove()
+	{
+		var direction = new Vector2();
+		direction.X = Input.GetAxis("ui_left", "ui_right");
+		direction.Y = Input.GetAxis("ui_up", "ui_down");
+		direction.Normalized();
 		Position += direction * Speed / camera.Zoom;
 	}
 }
